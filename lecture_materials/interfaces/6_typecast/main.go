f292fac681a9b9da89bb47ab5f000a97ceb7f28a
Тимофей Разумов <t.e.razumov@tinkoff.ru>
2025-02-23 15:50:11 +0300

    fixes

--- lecture_materials/interfaces/6_typecast/main.go
+++ lecture_materials/interfaces/6_typecast/main.go
@@ -25,21 +25,25 @@ func main() {
 		fmt.Printf("unsuccessful typecast: '%s', %T\n", s, b)
 	}
 
+	si := superInt(5)
+	b = &si
+	// b = myStringer(&si)
+	// b = superInt(5)
 	/*
-		si := superInt(5)
-		b = &si
-		// b = myStringer(&si)
-		// b = superInt(5)
 		if i, ok := b.(int); ok {
 			fmt.Printf("successful typecast to int: %d\n", i)
-		} else if s, ok := b.(fmt.Stringer); ok {
+		}
+
+		if s, ok := b.(fmt.Stringer); ok {
 			fmt.Printf("successful typecast to Stringer: %v\n", s)
-		} else if s, ok := b.(myStringer); ok {
+		}
+
+		if s, ok := b.(myStringer); ok {
 			fmt.Printf("successful typecast to myStringer: %v\n", s)
-		} else if mi, ok := b.(superInt); ok {
+		}
+
+		if mi, ok := b.(superInt); ok {
 			fmt.Printf("successful typecast to myInt: %s\n", mi)
-		} else {
-			fmt.Printf("unsuccessful typecast for %T\n", b)
 		}
 	*/
 
